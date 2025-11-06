2fee28bd91dc6b132287ca38f10ce53c9293dd09
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-06 16:24:57 +0530

    - Created the "Poll" entity class.

---
+++ voting-app/src/main/java/com/project/voting_app/entity/Poll.java
@@ -0,0 +1,26 @@
+package com.project.voting_app.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Poll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+
+    @ElementCollection // this annotation will create an entity/table by name "poll_options" with "poll_id" and "options" columns.
+    private List<OptionVote> options = new ArrayList<>();
+
+//    @ElementCollection // this annotation will create an entity/table by name "poll_votes" with "poll_id" and "votes" columns.
+//    private List<Long> votes = new ArrayList<>();
+}
